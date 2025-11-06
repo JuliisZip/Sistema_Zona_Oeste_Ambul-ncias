7783eca7e13a4ea78d81604bb2c55b42136a97d0
Juliis.zip <125084517+JuliisZip@users.noreply.github.com>
2025-11-06 01:50:29 -0300

    Update Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,12 @@
 // Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAqO6bxuwg3nBov4qECxFpaZ8D9bEc_6FI",
-    authDomain: "teste-d208d.firebaseapp.com",
-    projectId: "teste-d208d",
-    storageBucket: "teste-d208d.firebasestorage.app",
-    messagingSenderId: "887353153867",
-    appId: "1:887353153867:web:e78d576fc400395b9170ce"
-};
-
-/* Configuração do Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAqO6bxuwg3nBov4qECxFpaZ8D9bEc_6FI",
     authDomain: "sistema-ambulancias-16536.firebaseapp.com",
     projectId: "sistema-ambulancias-16536",
     storageBucket: "sistema-ambulancias-16536.firebasestorage.app",
     messagingSenderId: "887353153867",
     appId: "1:887353153867:web:e78d576fc400395b9170ce"
-};*/
+};
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
